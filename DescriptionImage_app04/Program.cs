b1d22e842ba67fa67f56382a2ba4e8afd1708d3c
Moyses Aguagallo <leo.aigaje12@gmail.com>
2021-01-13 12:03:00 -0500

    Agregar archivos de proyecto.

---
+++ DescriptionImage_app04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DescriptionImage_app04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = new System.Net.WebClient();
+            var url = "https://southcentralus.api.cognitive.microsoft.com/";
+
+            api.Headers.Add("Content-Type", "application/json");
+            api.Headers.Add("Ocp-Apim-Subscription-key", "your-key");
+
+            var qry = "maxCandidates=1&language=es";
+
+            var img = "https://www.caracteristicas.co/wp-content/uploads/2018/08/bosque-1-e1577589217640.jpg";
+
+            var json = "{'url':'" + img + "'}";
+
+            url = $"{url}/vision/v3.0/describe?{qry}";
+
+            var res = api.UploadString(url, "POST", json);
+            Console.WriteLine(res);
+
+
+        }
+    }
+}
